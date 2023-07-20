1971cf7ad1eafa4c4408085881d745d68551bbe0
Thiago F <40269174+wheregoes@users.noreply.github.com>
2023-07-20 16:22:46 -0300

    Major update to dmf.py
    Major update to dmf.py
    - Add args to multithreading in background.

--- dmf.py
+++ dmf.py
@@ -3,8 +3,10 @@ import requests
 import time
 import logging
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import argparse
 
-def download_file_with_retry(url, destination_folder):
+def download_file(url, destination_folder):
     retries = 3
     delay_times = [5, 30, 60]
 
@@ -20,11 +22,19 @@ def download_file_with_retry(url, destination_folder):
                 progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=file_name)
 
                 with open(destination_path, 'wb') as file:
+                    downloaded_size = 0
                     for data in response.iter_content(block_size):
                         progress_bar.update(len(data))
                         file.write(data)
+                        downloaded_size += len(data)
 
                 progress_bar.close()
+
+                # Verify integrity after download
+                if downloaded_size != total_size:
+                    print(f"Failed to download the complete file: {file_name}")
+                    return False
+
                 print(f"File downloaded: {file_name}")
                 return True
             else:
@@ -43,7 +53,15 @@ def download_file_with_retry(url, destination_folder):
 
     return False
 
-def download_files_from_file(file_path, destination_folder):
+def download_files_with_threading(urls_to_download, destination_folder, num_threads):
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = [executor.submit(download_file, url, destination_folder) for url in urls_to_download]
+
+        for future in as_completed(futures):
+            if not future.result():
+                print(f"Failed to download: {future.result()}")
+
+def download_files_from_file(file_path, destination_folder, num_threads):
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
@@ -61,29 +79,37 @@ def download_files_from_file(file_path, destination_folder):
         with open(file_path, 'w') as file:
             file.write('\n'.join(urls_to_download))
 
-    for url in urls_to_download:
-        success = download_file_with_retry(url, destination_folder)
-        if not success:
-            print(f"Failed to download: {url}")
+    download_files_with_threading(urls_to_download, destination_folder, num_threads)
 
-if __name__ == "__main__":
+def main():
     print(r"""
-__/\\\\\\\\\\\\_____/\\\\____________/\\\\__/\\\\\\\\\\\\\\\_        
- _\/\\\////////\\\__\/\\\\\\________/\\\\\\_\/\\\///////////__       
-  _\/\\\______\//\\\_\/\\\//\\\____/\\\//\\\_\/\\\_____________      
-   _\/\\\_______\/\\\_\/\\\\///\\\/\\\/_\/\\\_\/\\\\\\\\\\\_____     
-    _\/\\\_______\/\\\_\/\\\__\///\\\/___\/\\\_\/\\\///////______    
-     _\/\\\_______\/\\\_\/\\\____\///_____\/\\\_\/\\\_____________   
-      _\/\\\_______/\\\__\/\\\_____________\/\\\_\/\\\_____________  
-       _\/\\\\\\\\\\\\/___\/\\\_____________\/\\\_\/\\\_____________ 
-        _\////////////_____\///______________\///__\///______________
-                                      
-    """)
-    print("Download Multiple Files \n")
+    __/\\\\\\\\\\\\_____/\\\\____________/\\\\__/\\\\\\\\\\\\\\\_        
+     _\/\\\////////\\\__\/\\\\\\________/\\\\\\_\/\\\///////////__       
+      _\/\\\______\//\\\_\/\\\//\\\____/\\\//\\\_\/\\\_____________      
+       _\/\\\_______\/\\\_\/\\\\///\\\/\\\/_\/\\\_\/\\\\\\\\\\\_____     
+        _\/\\\_______\/\\\_\/\\\__\///\\\/___\/\\\_\/\\\///////______    
+         _\/\\\_______\/\\\_\/\\\____\///_____\/\\\_\/\\\_____________   
+          _\/\\\_______/\\\__\/\\\_____________\/\\\_\/\\\_____________  
+           _\/\\\\\\\\\\\\/___\/\\\_____________\/\\\_\/\\\_____________ 
+            _\////////////_____\///______________\///__\///______________
+                                                    
+        """)
+    
+    parser = argparse.ArgumentParser(description="Download Multiple Files", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--multi-thread", action="store_true", help="Use multi-threaded download")
+    parser.add_argument("-T", "--threads", type=int, default=4, help="Number of threads for multi-threaded download (default=4)")
+    args = parser.parse_args()
+
     # File containing URLs to download, one URL per line
     urls_file_path = "urls_to_download.txt"
 
     # Destination folder where files will be saved
     destination_folder = "downloads"
 
-    download_files_from_file(urls_file_path, destination_folder)
+    if args.multi_thread:
+        download_files_from_file(urls_file_path, destination_folder, args.threads)
+    else:
+        download_files_from_file(urls_file_path, destination_folder, 1)  # Set num_threads to 1 for File-per-File download
+
+if __name__ == "__main__":
+    main()
